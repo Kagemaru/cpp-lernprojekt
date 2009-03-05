0a6e13749ae2b0f8fb6bd5106a38da8f5c192ea5
Thomas Burkhalter <kagemaru@MCPC.(none)>
2009-03-05 09:55:31 +0100

    Beginning of the Parser class Started coding the Parser class and cleaned up the code a bit. Remember: Ask Ben about the Epsilon/End of String cases.

--- lexer.cpp
+++ lexer.cpp
@@ -5,31 +5,26 @@
 
 using namespace std;
 
+enum Token{ ADD, SUB, MUL, DIV, L_PAR, R_PAR, NUM, VAR };
 
 class Lexer{
     private:
-        enum Token{ ADD, SUB, MUL, DIV, L_PAR, R_PAR, NUM, VAR };
-        string calc;
-        int pos=0;
-        int numberValue;
-        string stringValue;
-        Token tok;
+        string  calc;
+        string  stringValue;
+        int     pos=0;
+        int     numberValue;
+        Token   tok;
         
     public:
-        Lexer(string str){ calc = str;  };
-        int getNumberValue(){ return numberValue;}
-        string getStringValue(){ return stringValue;}
-        bool hasNextToken(){
-            if(pos<calc.size()) { return true;  }
-            else                { return false; }
-        }
-
-        Token getToken(){ return tok; }
-
-        Token moveToNextToken(){
+                Lexer(string str)   { calc = str;               }
+        string  getStringValue()    { return stringValue;       }
+        int     getNumberValue()    { return numberValue;       }
+        bool    hasNextToken()      { return (pos<calc.size);   }
+        Token   getToken()          { return tok;               }
+        Token   moveToNextToken()   {
             char ch = calc[pos];
-            while(ch == ' ') { ch = calc[pos++]; }
-            switch(ch){
+            while (ch == ' ') { ch = calc[pos++]; }
+            switch (ch){
                 case '+': tok = ADD;
                 case '-': tok = SUB;
                 case '*':
@@ -39,18 +34,18 @@ class Lexer{
                 case '(': tok = L_PAR;
                 case ')': tok = R_PAR;
                 default:
-                    if(isdigit(ch)) {
+                    if (isdigit(ch)) {
                         int i = --pos;
                         numberValue = 0;
-                        while((i<calc.size()) && (isdigit(calc[i]))){
+                        while ((i<calc.size()) && (isdigit(calc[i]))){
                             numberValue = ((numberValue * 10) + calc[i++] - '0');
                         }
                         pos = i;
                         tok = NUM;
-                    } else if(isalpha(ch)) {
+                    } else if (isalpha(ch)) {
                         int i = --pos;
                         stringValue = "";
-                        while((i<calc.size()) && (isalpha(calc[i]))){
+                        while ((i<calc.size()) && (isalpha(calc[i]))){
                             stringValue += calc[i++];
                         }
                         tok = VAR;
@@ -60,9 +55,9 @@ class Lexer{
 
         Token getNextToken(){
             char ch = calc[pos++];
-            while(ch == ' ') { ch = calc[pos++]; }
+            while (ch == ' ') { ch = calc[pos++]; }
             cout << "CHAR: " << ch << endl;
-            switch(ch){
+            switch (ch){
                 case '+': return ADD;
                 case '-': return SUB;
                 case '*':
@@ -72,7 +67,7 @@ class Lexer{
                 case '(': return L_PAR;
                 case ')': return R_PAR;
                 default:
-                    if(isdigit(ch)) {
+                    if (isdigit(ch)) {
                         int i = --pos;
                         numberValue = 0;
                         while((i<calc.size()) && (isdigit(calc[i]))){
@@ -80,17 +75,61 @@ class Lexer{
                         }
                         pos = i;
                         return NUM;
-                    } else if(isalpha(ch)) {
+                    } else if (isalpha(ch)) {
                         int i = --pos;
                         stringValue = "";
                         while((i<calc.size()) && (isalpha(calc[i]))){
                             stringValue += calc[i++];
                         }
                         return VAR;
+                    } else {
+                        if (--pos >= calc.size){
+                            return false;
+                        }
                     }
-                    
             }
         }
+}
+
+class Parser {
+    private:
+        /*
+        expr  = NUM expr2
+              | VAR expr2
+              | PAR_L expr PAR_R
+        */
+        bool expr(){
+            Lexer::moveToNextToken();
+            if (tok = Lexer::getToken()){
+            
+            }
+            switch (tok){
+                case NUM:
+                    // Lexer::getNumberValue;
+                    return expr2();
+                case VAR:
+                    // Lexer::getStringValue;
+                    return expr2();
+                case PAR_L:
+                    if (expr2()){
+                        Lexer::moveToNextToken();
+                        tok = Lexer::getToken();
+                        return (tok == PAR_R);
+                    }
+                default: return true;
+            }
+        }
+        /*
+        expr2 = ADD expr expr2
+              | SUB expr expr2
+              | MUL expr expr2
+              | DIV expr expr2
+              | epsilon
+        */
+        bool expr2(){
+            
+        }
+    public:
 }
 
 int main(){
